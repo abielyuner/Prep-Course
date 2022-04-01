80e16fa0cf9daa13229a2ebfd644861619c16982
abielyuner <abielyunr1994@gmail.com>
2022-04-01 05:51:24 -0500

    segundo test JSII.test.js

---
+++ 03-JS-II/homework/Respuesta.js
@@ -0,0 +1,11 @@
+// for: es un bucle que se ejecuta de manera automatica, mientras se 
+//      cumpla la condicion.
+
+// &&: es un operador logico que se usa para forzar a la condicion  
+//      que se cumplan si o si toda la condion, para devolverte true, de lo contrario te devolvera false:
+
+// ||: es un operador logico que se usa para decirle a la condicion,
+//       que cumpla una parte para devolcerte true de lo contrario te devolvera
+//      false.
+
+// ! :  es un operador logico que se usa para negar a la condicion.
